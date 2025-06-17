c73a5ef23ec86bb2f4bc8b02cc76ebcdd9a21d79
KevalVala05 <keval.vala.05.08@gmail.com>
2025-06-17 15:16:48 +0530

    articles page building currently

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -8,6 +8,8 @@ import { GithubIcon } from "@/components/Icons";
 import { motion } from "framer-motion";
 import project1 from "../../public/images/projects/crypto-screener-cover-image.jpg";
 
+const FramerImage = motion(Image);
+
 const FeaturedProject = ({ type, title, summary, img, link, github }) => {
   return (
     <>
@@ -22,7 +24,10 @@ const FeaturedProject = ({ type, title, summary, img, link, github }) => {
           target="_blank"
           className="w-1/2 cursor-pointer overflow-hidden rounded-lg"
         >
-          <Image src={img} alt={title} className="w-full h-auto" />
+          <FramerImage src={img} alt={title} className="w-full h-auto"
+           whileHover={{scale:1.05}}
+          transition={{duration:0.2}}
+          />
         </Link>
 
         <div className="w-1/2 flex flex-col items-start justify-between pl-6">
@@ -64,7 +69,10 @@ const Project = ({ title, type, img, link, github }) => {
         target="_blank"
         className="w-full cursor-pointer overflow-hidden rounded-lg"
       >
-        <Image src={img} alt={title} className="w-full h-auto" />
+        <FramerImage
+         whileHover={{scale:1.05}}
+          transition={{duration:0.2}}
+        src={img} alt={title} className="w-full h-auto" />
       </Link>
 
       <div className="w-full flex flex-col items-start justify-between mt-4">
